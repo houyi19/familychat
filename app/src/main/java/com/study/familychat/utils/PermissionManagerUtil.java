bcd59cef0e81b490d0e05db6ed42ac8fc8ed5e06
houyi <houyisi4797sogou-inccom@houyi.sogou-inc.com>
2018-12-10 19:02:45 +0800

    add permission Util

---
+++ app/src/main/java/com/study/familychat/utils/PermissionManagerUtil.java
@@ -0,0 +1,14 @@
+package com.study.familychat.utils;
+
+import android.support.v4.app.FragmentActivity;
+
+public class PermissionManagerUtil {
+
+    public static void permission() {
+
+    }
+
+    public void requestPermissionResult(FragmentActivity activity,int result) {
+
+    }
+}
