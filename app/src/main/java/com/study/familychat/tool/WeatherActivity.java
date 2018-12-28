bd2534f0b8d5565880d636d718e632de2951d630
yi.hou <yi.hou@cootek.cn>
2018-12-28 22:33:39 +0800

    develop map function

--- app/src/main/java/com/study/familychat/tool/WeatherActivity.java
+++ app/src/main/java/com/study/familychat/tool/WeatherActivity.java
@@ -4,9 +4,15 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.FragmentActivity;
 
+import com.amap.api.maps2d.AMap;
+import com.amap.api.maps2d.LocationSource;
+import com.amap.api.maps2d.MapView;
 import com.study.familychat.R;
 
-public class WeatherActivity extends FragmentActivity {
+public class WeatherActivity extends FragmentActivity implements LocationSource {
+    private MapView mMap;
+    private AMap aMap;
+
 
     public WeatherActivity() {
         super();
@@ -16,21 +22,51 @@ public class WeatherActivity extends FragmentActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.act_tool_weather);
-
+        mMap = findViewById(R.id.act_tool_map);
+        mMap.onCreate(savedInstanceState);
+        if (aMap == null) {
+            aMap = mMap.getMap();
+        }
+        aMap.setLocationSource(this);
+        aMap.setMyLocationEnabled(true);
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
+        mMap.onDestroy();
     }
 
     @Override
     protected void onResume() {
         super.onResume();
+        mMap.onResume();
     }
 
     @Override
     protected void onStart() {
         super.onStart();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mMap.onResume();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        mMap.onSaveInstanceState(outState);
+    }
+
+    @Override
+    public void activate(OnLocationChangedListener onLocationChangedListener) {
+
+    }
+
+    @Override
+    public void deactivate() {
+
     }
 }
